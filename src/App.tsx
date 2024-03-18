63d3d10b886b3435750afd1439a05c8506b5e70c
Joe Yates <joe.g.yates@gmail.com>
2024-03-18 12:59:16 +0100

    Show chat UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import logo from './assets/logo.svg'
 import './App.css'
 import './style.css'
 
-import { Example } from './Example'
+import {Chat} from './Chat'
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Example />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <Chat />
       </header>
     </div>
-  );
+  )
 }
---
+++ src/Chat.tsx
@@ -0,0 +1,111 @@
+import {useEffect, useState} from 'react'
+import {LIB_VERSION} from 'electric-sql/version'
+import {makeElectricContext, useLiveQuery} from 'electric-sql/react'
+import {genUUID, uniqueTabId} from 'electric-sql/util'
+import {Electric, Messages as Message, schema} from './generated/client'
+import {ElectricDatabase, electrify} from 'electric-sql/wa-sqlite'
+
+import {authToken} from './auth'
+
+const {ElectricProvider, useElectric} = makeElectricContext<Electric>()
+
+const Chat = () => {
+  const [electric, setElectric] = useState<Electric>()
+
+  useEffect(() => {
+    let isMounted = true
+
+    const init = async () => {
+      const config = {
+        debug: import.meta.env.DEV,
+        url: import.meta.env.ELECTRIC_SERVICE
+      }
+
+      const {tabId} = uniqueTabId()
+      const scopedDbName = `basic-${LIB_VERSION}-${tabId}.db`
+
+      const conn = await ElectricDatabase.init(scopedDbName)
+      const electric = await electrify(conn, schema, config)
+      await electric.connect(authToken())
+
+      if (!isMounted) {
+        return
+      }
+
+      setElectric(electric)
+    }
+
+    init()
+
+    return () => {
+      isMounted = false
+    }
+  }, [])
+
+  if (electric === undefined) {
+    return null
+  }
+
+  return (
+    <ElectricProvider db={electric}>
+      <ChatComponent />
+    </ElectricProvider>
+  )
+}
+
+const ChatComponent = () => {
+  const {db} = useElectric()!
+  const {results} = useLiveQuery(db.messages.liveMany())
+  const [text, setText] = useState('')
+
+  useEffect(() => {
+    const syncMessages = async () => {
+      const shape = await db.messages.sync()
+
+      await shape.synced
+    }
+
+    syncMessages()
+  }, [])
+
+  const addMessage = async () => {
+    await db.messages.create({
+      data: {
+        id: genUUID(),
+        text
+      }
+    })
+    setText('')
+  }
+
+  const clearMessages = async () => {
+    await db.messages.deleteMany()
+  }
+
+  const onChange: React.ChangeEventHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value)
+  }
+
+  const messages: Message[] = results ?? []
+
+  return (
+    <div>
+      {messages.map((message: Message) => (
+        <p key={message.id} className='message'>
+          <div>{message.text}</div>
+        </p>
+      ))}
+      <div className='controls'>
+        <input type='text' value={text} onChange={onChange} />
+        <button className='button' onClick={addMessage}>
+          Add
+        </button>
+        <button className='button' onClick={clearMessages}>
+          Clear
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export {Chat}
