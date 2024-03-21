4994dc60835fe8b8dc54a1e3deed343715aa25f4
Joe Yates <joe.g.yates@gmail.com>
2024-03-21 09:49:15 +0100

    Remove unused import

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useMemo, useState} from 'react'
+import {useEffect, useState} from 'react'
 import {useLiveQuery} from 'electric-sql/react'
 import {genUUID} from 'electric-sql/util'
 
